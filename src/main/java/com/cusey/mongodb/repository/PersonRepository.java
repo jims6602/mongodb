6418b444ab8c7779799b2fffddb9d0d2fb4e7c9d
jims6602 <112780571+jims6602@users.noreply.github.com>
2022-09-04 01:30:55 -0400

    Add files via upload

---
+++ src/main/java/com/cusey/mongodb/repository/PersonRepository.java
@@ -0,0 +1,15 @@
+package com.cusey.mongodb.repository;
+
+import com.cusey.mongodb.models.Person;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PersonRepository extends MongoRepository<Person,String> {
+
+    public Person findByFirstName(String firstName);
+    public List<Person> findByAge(int age);
+
+}
